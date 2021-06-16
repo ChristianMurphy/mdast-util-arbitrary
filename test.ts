87e072325f6b03ffd0ca2551bef8c24c59d986ac
Christian Murphy <christian.murphy.42@gmail.com>
2021-06-16 10:56:51 -0700

    test: add remark lint to test suite

--- test.ts
+++ test.ts
@@ -1,34 +1,73 @@
 import { test } from "uvu";
 // @ts-ignore cjs-esm conflict
-import toString from "mdast-util-to-markdown";
+import toMarkdownText from "mdast-util-to-markdown";
+// @ts-ignore cjs-esm conflict
+import remark from "remark";
+// @ts-ignore no typings
+import lintRecommended from "remark-preset-lint-recommended";
+// @ts-ignore no typings
+import lintStyleguide from "remark-preset-lint-markdown-style-guide";
+// @ts-ignore no typings
+import lintConsistent from "remark-preset-lint-consistent";
+
 import fc from "fast-check";
 import { commonmark } from "./index.js";
 
-test("no options", () => {
+test("no options with mdast-util-to-markdown", () => {
   fc.assert(
     fc.property(commonmark().Root, (mdast) => {
-      const markdown = toString(mdast);
+      const markdown = toMarkdownText(mdast);
       return typeof markdown === "string" && markdown.length > 1;
     }),
-    { numRuns: 10 }
+    { numRuns: 10,  }
   );
 });
 
-test("rootNodeMaxChildren", () => {
+test("rootNodeMaxChildren with mdast-util-to-markdown", () => {
   fc.assert(
     fc.property(commonmark({ rootNodeMaxChildren: 10 }).Root, (mdast) => {
-      const markdown = toString(mdast);
+      const markdown = toMarkdownText(mdast);
       return typeof markdown === "string" && markdown.length > 1;
     }),
     { numRuns: 10 }
   );
 });
 
-test("includeData", () => {
+test("includeData with mdast-util-to-markdown", () => {
   fc.assert(
     fc.property(commonmark({ includeData: true }).Root, (mdast) => {
-      const markdown = toString(mdast);
+      const markdown = toMarkdownText(mdast);
       return typeof markdown === "string" && markdown.length > 1;
+    }),
+    { numRuns: 10 }
+  );
+});
+
+test("commonmark with remark-preset-lint-recommended", () => {
+  fc.assert(
+    fc.property(commonmark({ includeData: true }).Root, (mdast) => {
+      remark().use(lintRecommended).runSync(mdast);
+      return true;
+    }),
+    { numRuns: 10 }
+  );
+});
+
+test("commonmark with remark-preset-lint-markdown-style-guide", () => {
+  fc.assert(
+    fc.property(commonmark({ includeData: true }).Root, (mdast) => {
+      remark().use(lintStyleguide).runSync(mdast);
+      return true;
+    }),
+    { numRuns: 10 }
+  );
+});
+
+test("commonmark with remark-preset-lint-consistent", () => {
+  fc.assert(
+    fc.property(commonmark({ includeData: true }).Root, (mdast) => {
+      remark().use(lintConsistent).runSync(mdast);
+      return true;
     }),
     { numRuns: 10 }
   );
